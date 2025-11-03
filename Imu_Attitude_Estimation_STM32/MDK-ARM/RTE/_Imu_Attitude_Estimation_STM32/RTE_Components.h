b3ab9554e6c06f637e037429bfd04f9e9c50d18c
NhatTran-97 <nhat.tranduy1997@gmail.com>
2025-11-03 09:37:28 +0700

    Added project

---
+++ Imu_Attitude_Estimation_STM32/MDK-ARM/RTE/_Imu_Attitude_Estimation_STM32/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Imu_Attitude_Estimation_STM32' 
+ * Target:  'Imu_Attitude_Estimation_STM32' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
